2743ca81a5194fa5ce464f172249846c542b43f8
aminKhaleghi94 <aminkhalegh1373@gmail.com>
2020-02-03 22:41:02 +0330

    Create urls.py

---
+++ catalog/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url, include
+from django.urls import re_path
+
+from . import views
+
+app_name = 'catalog'
+
+urlpatterns = [
+    url(r'^$',views.Index.as_view(), name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^authors/(?P<pk>\d+)/$', views.AuthorDetailView.as_view(), name='author_detail'),
+    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+    #APi Url For Book
+    re_path(r'^get/$', views.APIListCreateBook.as_view(), name = 'apiBooklist'),
+    re_path(r'^update/(?P<pk>\d+)/$', views.APIRetrieveUpdateDestroyBook.as_view(), name = 'apiBookUpdate'),
+]
